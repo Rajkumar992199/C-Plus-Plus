2072a3e987345e10fb2bfdfe1fe5e7d8678151dd
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-01-14 08:45:47 +0000

    1030A_In_Search_of_an_Easy_Problem

---
+++ codeforces/800/1030A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,k=0;
+    cin >> n ;
+    int a[n];
+    for(int i=0; i<n;i++){
+        cin >> a[i];
+        if(a[i]==1) k++;
+    }
+    if(k>0) cout << "HARD";
+    else cout << "EASY";
+}
