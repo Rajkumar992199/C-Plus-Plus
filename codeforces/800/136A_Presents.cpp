4bc74eb9de1e34380d972a0949621b7f9df22c7f
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-01-15 19:29:55 +0000

    136A_Presents

---
+++ codeforces/800/136A_Presents.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int p[n],a[n];
+    for(int i=0;i<n;i++) cin >> p[i];
+    for(int i=0;i<n;i++) a[p[i]-1]= i+1;
+    for(int i=0;i<n;i++) cout << a[i] << " ";
+}
