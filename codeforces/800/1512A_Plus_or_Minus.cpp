f8e11fe825a59c6f5a15d8f06a9b2dcb4724b2a5
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-01-31 16:32:34 +0000

    1512A_Plus_or_Minus

---
+++ codeforces/800/1512A_Plus_or_Minus.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,a,b,c;
+    cin >> t;
+    for(int j=0;j<t;j++){
+        cin >> a >> b >> c;
+        if(a+b==c) cout << "+" << endl;
+        else cout << "-" << endl;
+    }
+}
