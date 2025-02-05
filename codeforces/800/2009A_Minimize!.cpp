5335d610b2aad7d79ac445ea95d7b3259c0496de
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-02-05 10:47:33 +0000

    2009A_Minimize!

---
+++ codeforces/800/2009A_Minimize!.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,a,b,c;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        cin >> a >> b;
+        c = (a+b)/2;
+        cout << (c-a)+(b-c) << endl;
+    }
+}
