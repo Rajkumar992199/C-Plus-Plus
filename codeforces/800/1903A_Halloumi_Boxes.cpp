758c5b8b45cbcc32fc5baf1f815ba0fb8a1edbdd
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-02-19 17:07:08 +0000

    1903A_Halloumi_Boxes

---
+++ codeforces/800/1903A_Halloumi_Boxes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,n,k;
+    cin >> t;
+    while(t--){
+        cin >> n >> k;
+        int a[n];
+        for(int i=0;i<n;i++) cin >> a[i];
+        if(is_sorted(a,a+n) || k>1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
