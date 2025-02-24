834aca8762d74af7c1ba739a2460f0f5524d276a
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-02-24 17:04:40 +0000

    1866A_Ambitious_Kid

---
+++ codeforces/800/1866A_Ambitious_Kid.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+        if(a[i]<0) a[i] = 0-a[i];
+    }
+    cout << *min_element(a,a+n) << endl;
+}
