7e0d05554f96ad628e197b9c258bb9bfe8ddabce
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-04-05 04:19:51 +0000

    1904A_Forked!

---
+++ codeforces/900/1904A_Forked!.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {-1, 1, -1, 1}, dy[4] = {-1, -1, 1, 1};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, x_king, y_king, x_queen, y_queen;
+        cin >> a >> b >> x_king >> y_king >> x_queen >> y_queen;
+        set<pair<int,int>> king_hits, queen_hits;
+        for (int i = 0; i < 4; i++) {
+            king_hits.insert({x_king + dx[i] * a, y_king + dy[i] * b});
+            king_hits.insert({x_king + dx[i] * b, y_king + dy[i] * a});
+            queen_hits.insert({x_queen + dx[i] * a, y_queen + dy[i] * b});
+            queen_hits.insert({x_queen + dx[i] * b, y_queen + dy[i] * a});
+        }
+        int ans = 0;
+        for (auto position : king_hits) if (queen_hits.find(position) != queen_hits.end()) ans++;
+        cout << ans << endl;
+    }
+    return 0;
+}
