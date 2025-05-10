1506d51256442042e412ae9953f33b90d1afdfd8
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-05-10 10:06:01 +0000

    corrected 702A_Maximum_Increase

--- codeforces/800/702A_Maximum_Increase.cpp
+++ codeforces/800/702A_Maximum_Increase.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main() {
     long long n;
     cin >> n;
-    long long a[n],max=0,count=1;
-    for(int i=0;i<n;i++) cin >> a[i];
-    for(int i=1;i<n;i++) {
-        if(a[i-1]<a[i]) count++;
-        else count=1;
-        if(count > max ) max = count;
+    long long a[n], max = 1, count = 1;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 1; i < n; i++) {
+        if (a[i - 1] < a[i]) {
+            count++;
+            if(count > max ) max = count;
+        }
+        else count = 1;
     }
     cout << max;
 }
