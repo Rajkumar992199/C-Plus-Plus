55c9138e7ec29f9a34654b888f3458a6c28ac658
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-05-13 04:39:15 +0000

    removed duplicate

--- codeforces/900/479A_Expression.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int a,b,c,k;
-    cin >> a >> b >> c;
-    k=max(max(max(a+b+c,(a+b)*c),max(a*(b+c),a*b*c)),max(a+(b*c),(a*b)+c));
-    cout << k;
-}
