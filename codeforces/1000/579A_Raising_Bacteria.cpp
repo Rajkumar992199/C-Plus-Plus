2fb0cf15f2c7dc21be2c1456c058ff9c5447b56d
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-05-17 15:44:20 +0000

    579A_Raising_Bacteria

---
+++ codeforces/1000/579A_Raising_Bacteria.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x, ans = 0;
+    cin >> x;
+    while(x > 0) {
+        if (x % 2 == 1) ans++;
+        x /= 2;
+    }
+    cout << ans;
+    return 0;
+}
