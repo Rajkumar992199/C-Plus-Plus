e7ddd20b9ccf148768b22105234098f6d6473885
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-05-26 14:49:50 +0000

    25A_IQ_test

---
+++ codeforces/1300/25A_IQ_test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 1; i < n; i++) if ( (a[i-1] - a[i]) % 2 != 0) {
+        if ( ((a[i] - a[i + 1]) % 2 != 0 )|| (i == n - 1 )) cout << i + 1;
+        else cout << i;
+        break;
+    }
+}
