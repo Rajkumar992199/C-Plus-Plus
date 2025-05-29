2bca4e71edc6181201119c39227df9068621466f
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-05-29 15:58:25 +0000

    2114A_Square_Year

---
+++ codeforces/800/2114A_Square_Year.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int sq = floor(sqrt(n));
+        if (sq * sq == n) cout << 0 << " " << sq << endl;
+        else cout << -1 << endl;
+    }
+    return 0;
+}
