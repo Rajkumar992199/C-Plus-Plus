24767383a2780a082f565c14bea6b26476d52741
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-08-20 15:42:00 +0000

    1560A - Dislike of Threes

---
+++ codeforces/800/1560A_Dislike_of_Threes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, ans = 0;
+        cin >> n;
+        for(int i = 1; ;i++) {
+            if(i % 3 == 0 || i % 10 == 3) continue;
+            if(n == 1) {
+                cout << i << endl; 
+                break;
+            }
+            n--;
+        }
+    }
+    return 0;
+}
