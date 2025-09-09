491398da20bf5c858ddfb605926ae5a744ef94dc
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-09-09 16:46:30 +0000

    START202 Endless Play

---
+++ Codechef/START202 Endless Play.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X, H, ans;
+    cin >> X >> H;
+    ans = (X - 4) * 24 + H;
+    cout << ans << endl;
+}
