dacd998606f83e29cbbeac5ffda409d3fb70a8e4
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-09-16 17:41:54 +0000

    Greater Average

---
+++ Codechef/Greater Average.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin >> T;
+    while(T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        if (A + B > 2 * C) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
