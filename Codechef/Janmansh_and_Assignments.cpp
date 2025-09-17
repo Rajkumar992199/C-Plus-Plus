cbb6668f57e7c5a4236fcca22ea2684772f9cbaf
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-09-17 17:49:20 +0000

    Janmansh_and_Assignments

---
+++ Codechef/Janmansh_and_Assignments.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin >> T;
+    while(T--) {
+        int X;
+        cin >> X;
+        if(X > 7) cout << "NO\n";
+        else cout << "YES\n";
+    }
+    return 0;
+}
