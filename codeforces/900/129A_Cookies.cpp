7a9206f2e151d630cb3bdf6c1a53dbbb3e247ab4
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-09-19 18:13:51 +0000

    129A_Cookies

---
+++ codeforces/900/129A_Cookies.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int sum = 0, cnt = 0;
+    for (int i=0;i<n;i++){
+        cin >> a[i];
+        sum += a[i];
+    }
+    for (int i = 0; i < n; i++){
+        if ((sum-a[i])%2==0)
+            cnt++; 
+    }
+    cout << cnt << endl;
+    return 0;
+}
