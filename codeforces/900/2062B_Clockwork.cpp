0caa2296889bd8264fc0d5f27370d2c988d492d6
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-10-06 15:26:32 +0000

    2062B_Clockwork

---
+++ codeforces/900/2062B_Clockwork.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        if (a[i] <= 2 * max(i, n - 1 - i)) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+        solve();
+    return 0;
+}
