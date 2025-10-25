62e1d46bc4cdbe2f1f6be107ea1971e0709f946b
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-10-25 06:27:46 +0000

    Good Turn

---
+++ Codechef/Good Turn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        // your code goes here
+        if(x + y > 6) cout << "YES\n";
+        else cout << "NO\n";
+    }
+	return 0;
+}
