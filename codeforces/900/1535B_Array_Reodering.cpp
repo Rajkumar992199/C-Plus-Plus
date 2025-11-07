1e1c654223824afc2271709c595bbc0abcc307a8
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-11-07 18:03:16 +0000

    1535B_Array_Reodering

---
+++ codeforces/900/1535B_Array_Reodering.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+    sort(a.begin(), a.end(), [](int x, int y) {
+      return x % 2 < y % 2;
+    });
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        ans += __gcd(a[i], a[j] * 2) > 1;
+      }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
