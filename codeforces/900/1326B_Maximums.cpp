76d5a5d4e8852c6036d5b24f0ee59c7f4e7e14a2
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-11-10 18:31:54 +0000

    1326B_Maximums

---
+++ codeforces/900/1326B_Maximums.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0); cout.tie(0);
+    
+    long long n, a, sum = 0, x = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      cin >> a;
+      a += x;
+      x = max(x, a);
+      cout << a << ' ';
+    }
+    return 0;
+}
