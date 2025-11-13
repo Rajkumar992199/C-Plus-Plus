090f4039e79cb57d7167d42efc8d484d546ac404
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2025-11-13 18:24:27 +0000

    2167C_Isamatdin_and_His_Magic_Wand!

---
+++ codeforces/800/2167C_Isamatdin_and_His_Magic_Wand!.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+    bool o = 0, e = 0;
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    flp(0, n)
+    {
+        cin >> a[i];
+        if(a[i] % 2)
+            o = 1;
+        else
+            e = 1;
+    }
+    if(e && o)
+        sort(a.begin(), a.end());
+    flp(0, n)
+        cout << a[i] << " ";
+    NL;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
