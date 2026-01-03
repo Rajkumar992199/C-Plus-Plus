c7d70fd4672473162bbc0aaede6a8fb70fe862df
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-01-03 18:14:03 +0000

    4_Median_of_Two_Sorted_Arrays

---
+++ Leetcode/Hard/4_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+            nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+            sort(nums1.begin(), nums1.end());
+            for (int i = 0; i < nums1.size(); i++) cout << nums1[i];
+            double ans;
+            if (nums1.size() % 2 == 1) ans = nums1[(nums1.size() / 2)];
+            else ans = (nums1[nums1.size() / 2] + nums1[(nums1.size() / 2) - 1]) / 2.0;
+            return ans;
+        }
+    };
