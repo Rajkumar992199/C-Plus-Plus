1c6ce850023053c16320191d81579f22a193a140
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-01-13 17:30:09 +0000

    616A_Comparing_Two_Long_Integers

---
+++ codeforces/900/616A_Comparing_Two_Long_Integers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string a, b;
+    cin >> a >> b;
+    int s = 0;
+    while(s < (int)a.length() && a[s] == '0')
+        s++;
+    a = a.substr(s);
+    s = 0;
+    while(s < (int)b.length() && b[s] == '0')
+        s++;
+    b = b.substr(s);
+    if(a.length() != b.length()) {
+        if(a.length() > b.length())
+            cout << '>';
+        else
+            cout << '<';
+        return 0;
+    }
+    for(size_t i = 0; i < a.length(); i++) {
+        if(a[i] != b[i]) {
+            if(a[i] > b[i])
+                cout << '>';
+            else
+                cout << '<';
+            return 0;
+        }
+    }
+    cout << '=';
+    return 0;
+}
