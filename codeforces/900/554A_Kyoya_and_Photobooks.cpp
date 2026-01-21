9ce00ef54aefe682778276677c7f12b9f98e2c29
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-01-21 17:41:44 +0000

    554A_Kyoya_and_Photobooks

---
+++ codeforces/900/554A_Kyoya_and_Photobooks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    int n = 25 * s.size() + 26;
+    cout << n << endl;
+    
+    return 0;
+}
