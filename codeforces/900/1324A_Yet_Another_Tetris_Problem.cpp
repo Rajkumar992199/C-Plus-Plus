8abcb68c344ddcfdd63e284d09068363ebd49928
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-02-05 17:24:03 +0000

    1324A_Yet_Another_Tetris_Problem

---
+++ codeforces/900/1324A_Yet_Another_Tetris_Problem.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) 
+        cin >> a[i];
+    int pair = a[0] % 2;
+    for(int i = 0; i < n; i++) {
+        if(i && pair != (a[i] % 2)) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
