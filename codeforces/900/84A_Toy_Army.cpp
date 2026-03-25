325b158e170152aabb3c1713f4b73018f126eefa
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-03-25 06:44:37 +0000

    84A_Toy_Army

---
+++ codeforces/900/84A_Toy_Army.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	long long n;
+	cin >> n;
+	long long ans = 1.5 * n;
+	cout << ans;
+
+	return 0;
+}
