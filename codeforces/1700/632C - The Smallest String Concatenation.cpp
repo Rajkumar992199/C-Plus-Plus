1c2a377e03c0ae065ffd6818215902b9f4519680
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-04-25 23:59:09 +0530

    632C - The Smallest String Concatenation

---
+++ codeforces/1700/632C - The Smallest String Concatenation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(string a, string b) {
+    return a + b < b + a;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<string> v(n);
+
+    for(int i = 0; i < n; i++) 
+        cin >> v[i];
+
+    sort(v.begin(), v.end(), cmp);
+
+    for(string x : v) 
+        cout << x;
+    return 0;
+}
