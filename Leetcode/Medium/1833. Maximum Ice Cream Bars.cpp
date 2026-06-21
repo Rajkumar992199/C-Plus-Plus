632ad03816f7feef86063e6aee0c38e23b15b6d8
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-06-21 17:56:08 +0000

    1833. Maximum Ice Cream Bars

---
+++ Leetcode/Medium/1833. Maximum Ice Cream Bars.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int ans = 0;
+        sort(costs.begin(), costs.end());
+        for(auto it: costs) {
+            if(it > coins)
+                break;
+            coins -= it;
+            ans++;
+        }
+        return ans;
+    }
+};
