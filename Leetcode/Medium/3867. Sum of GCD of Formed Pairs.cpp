f1c0051fd154601ea21405e68bc9e65e98c40e68
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-07-24 18:38:00 +0000

    3867. Sum of GCD of Formed Pairs

---
+++ Leetcode/Medium/3867. Sum of GCD of Formed Pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int mx = 0, n = nums.size();
+        vector<int> pre(n);
+        for(int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+            pre[i] = __gcd(mx, nums[i]);
+        }
+        sort(pre.begin(), pre.end());
+        long long sum = 0;
+        for(int i = 0; i < n / 2; i++) 
+            sum += __gcd(pre[i], pre[n - i - 1]);
+        return sum;
+    }
+};
