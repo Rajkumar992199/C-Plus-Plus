86ac3e97d9ea7d872ad8db56d4e61bc6dca43c13
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-08-01 17:24:00 +0000

    1455C - Ping-pong

---
+++ codeforces/1100/1455C - Ping-pong.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    
+    cout << x-1 << ' ' << y << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--)
+        solve();
+}
