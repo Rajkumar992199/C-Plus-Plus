f85931314cce56e56722eb4c2af5360b9c367d3b
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-08-02 18:15:14 +0000

    242_Valid_Anagram update

--- Leetcode/Easy/242_Valid_Anagram.cpp
+++ Leetcode/Easy/242_Valid_Anagram.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] != t[i]) return false;
+        int n = s.size(), m = t.size();
+        if(n != m)
+            return false;
+
+        vector<int> freq1(26,0), freq2(26,0);
+        for(int i = 0; i < n; i++) {
+            freq1[s[i] - 'a']++;
+            freq2[t[i] - 'a']++;
         }
+
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] != freq2[i])
+                return false;
+        }
+        
         return true;
     }
 };
