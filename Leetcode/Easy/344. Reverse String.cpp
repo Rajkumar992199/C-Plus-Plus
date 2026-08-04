f039fac8672b0925a1b3e5e6a59b26829f4cd578
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2026-08-04 11:01:40 +0000

    344. Reverse String

---
+++ Leetcode/Easy/344. Reverse String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char c;
+        int n = s.size();
+        for(int i = 0; i < n/2; i++) {
+            c = s[i];
+            s[i] = s[n - i - 1];
+            s[n - i - 1] = c;
+        }
+    }
+};
